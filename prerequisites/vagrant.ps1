4c7ce9c3eb9668674107916a83ab1a6c1ba813c3
dpoisson <dominic.poisson@gmail.com>
2016-03-08 00:01:36 +0100

    Update vagrant.ps1

--- prerequisites/vagrant.ps1
+++ prerequisites/vagrant.ps1
@@ -1,4 +1,4 @@
 $source = "https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1.msi"
-$destination = "d:\workspace\install"
+$destination = "d:\workspace\install\vagrant" 
  
 Invoke-WebRequest $source -OutFile $destination
